84c85e5edcf13cccef89fa4e1cbfef4eca52c8a2
cyrilsl <cyrilsl@github.com>
2024-01-21 14:13:32 +0530

    first woking commit with induvidual stores sutogenerated on user creation

---
+++ src/api/middlewares.ts
@@ -0,0 +1,84 @@
+import {
+  authenticate,
+  type MedusaNextFunction,
+  type MedusaRequest,
+  type MedusaResponse,
+  type MiddlewaresConfig,
+  type UserService,
+} from '@medusajs/medusa';
+import cors from 'cors';
+import { User } from 'src/models/user';
+
+const registerLoggedInUser = async (req: MedusaRequest, res: MedusaResponse, next: MedusaNextFunction) => {
+  let loggedInUser: User | null = null;
+
+  if (req.user && req.user.userId) {
+    const userService = req.scope.resolve('userService') as UserService;
+    loggedInUser = await userService.retrieve(req.user.userId);
+  }
+
+  req.scope.register({
+    loggedInUser: {
+      resolve: () => loggedInUser,
+    },
+  });
+
+  next();
+};
+
+export const permissions = async (req: MedusaRequest, res: MedusaResponse, next: MedusaNextFunction) => {
+  if (!req.user || !req.user.userId) {
+    next();
+    return;
+  }
+  // retrieve currently logged-in user
+  const userService = req.scope.resolve('userService') as UserService;
+  const loggedInUser = await userService.retrieve(req.user.userId, {
+    select: ['id'],
+    relations: ['teamRole', 'teamRole.permissions'],
+  });
+
+  if (!loggedInUser.teamRole) {
+    // considered as super user
+    next();
+    return;
+  }
+
+  const isAllowed = loggedInUser.teamRole?.permissions.some((permission) => {
+    const metadataKey = Object.keys(permission.metadata).find((key) => key === req.path);
+    if (!metadataKey) {
+      return false;
+    }
+
+    // boolean value
+    return permission.metadata[metadataKey];
+  });
+
+  if (isAllowed) {
+    next();
+    return;
+  }
+
+  // deny access
+  res.sendStatus(401);
+};
+
+const corsOptions = {
+  origin: 'http://localhost:7001',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+export const config: MiddlewaresConfig = {
+  routes: [
+    {
+      matcher: /\/admin\/[^(auth)].*/,
+      middlewares: [cors(corsOptions), authenticate(), registerLoggedInUser],
+    },
+    // {
+    //   matcher: '/admin/*',
+    //   middlewares: [cors(corsOptions), authenticate(), permissions],
+    // },
+  ],
+};
---
+++ src/models/store.ts
@@ -0,0 +1,22 @@
+import { Store as MedusaStore } from '@medusajs/medusa';
+import { Entity, JoinColumn, OneToMany } from 'typeorm';
+import { Order } from './order';
+import { Product } from './product';
+import { Role } from './role';
+import { User } from './user';
+
+@Entity()
+export class Store extends MedusaStore {
+  @OneToMany(() => User, (user) => user?.store)
+  members?: User[];
+
+  @OneToMany(() => Product, (product) => product?.store)
+  products?: Product[];
+
+  @OneToMany(() => Order, (order) => order?.store)
+  orders?: Order[];
+
+  @OneToMany(() => Role, (role) => role.store)
+  @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+  roles: Role[];
+}
