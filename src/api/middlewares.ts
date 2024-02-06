bebfcbbd5e48e0fac1395a2377a2f3257f0de5ee
cyrilsl <cyrilsl@github.com>
2024-02-06 15:26:49 +0530

    removed localhost from middleware

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -76,7 +76,7 @@ export const permissions = async (
 
 
 const corsOptions = {
-  origin: process.env.ADMIN_CORS || 'http://localhost:7001' ,
+  origin: process.env.ADMIN_CORS,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
