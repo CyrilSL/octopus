273a7110ae3208964ba5c4f4be31c9043b185bed
cyrilsl <cyrilsl@github.com>
2024-02-06 14:06:51 +0530

    admin serve, only on serve: process.env.NODE_ENV === "development",

--- medusa-config.js
+++ medusa-config.js
@@ -45,7 +45,11 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: true,
+      // only enable `serve` in development
+      // you may need to add the NODE_ENV variable
+      // manually
+      serve: process.env.NODE_ENV === "development",
+      
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
